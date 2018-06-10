303df9b7e38bf53a5cb4b73b10a0e50fd4574947
Joseph McNeal Jr <joseph.mcneal@crackerbarrel.com>
2018-06-10 03:56:15 -0500

    Add project files.

---
+++ FilePurger/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FilePurger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
